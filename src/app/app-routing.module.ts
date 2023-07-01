58b4470b41a62ea738ccb36e1907dab2aba8feb4
Javier Alejandro Castañeda Celis <desarrollo@bitu.com.co>
2023-06-30 19:36:42 -0500

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './modules/pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent},
+/*   { path: 'changePaswordAuth/:id', component: ForgotChangePasswordComponent},
+  { path: 'mios', loadChildren: () => import('./mios/mios.module').then(m => m.MiosModule),
+    component: MiosComponent, canActivate: [AuthGuard], canActivateChild: [AuthGuard] 
+  },*/
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
